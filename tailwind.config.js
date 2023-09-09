ae7f6a3f10f7e0ede6d9145f0e6f32da07d8c126
Muhammad Iqbal <muhammadiqbalid83@gmail.com>
2023-09-09 08:37:20 +0700

    add a configure to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      orange: "#ff871d",
+      white: "#fff",
+      "base-border": "#e5e5e5",
+    },
   },
   plugins: [],
 };
